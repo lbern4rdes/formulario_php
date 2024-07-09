f8600366c93d809e436192e09fddc086e2072f1e
lbern4rdes <luisgbragabernardes@gmail.com>
2024-07-09 13:42:36 -0300

    Mais uma aula

---
+++ ex006/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste de calculo</title>
+</head>
+<body>
+    <?php 
+    $result = 2*(2 + 4);
+    
+    echo "Esse é o resultado = $result";
+    ?>
+</body>
+</html>
